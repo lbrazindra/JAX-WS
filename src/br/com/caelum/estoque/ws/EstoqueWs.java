aeddde32681ce7163e26189f258f71eb8339627f
Lucca <brazlucca@gmail.com>
2019-05-30 16:46:48 -0300

    TESTE

---
+++ src/br/com/caelum/estoque/ws/EstoqueWs.java
@@ -0,0 +1,49 @@
+package br.com.caelum.estoque.ws;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
+import br.com.caelum.estoque.modelo.item.Item;
+import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
+import br.com.caelum.estoque.usuario.AutorizacaoException;
+
+@WebService
+public class EstoqueWs {
+
+	private ItemDao dao = new ItemDao();
+
+	@WebMethod(operationName = "TodosOsItens")
+	@WebResult(name = "itens")
+	public ListaItens getItens(@WebParam(name = "filtros") Filtros filtros) {
+		System.out.println("Chamando getItens()");
+
+		List<Filtro> lista = filtros.getLista();
+		List<Item> itensResultado = dao.todosItens(lista);
+		
+		return new ListaItens(itensResultado);
+	}
+
+	@WebMethod(operationName = "CadastrarItem")
+	@WebResult(name = "item")
+	public Item cadastrarItem(@WebParam(name = "tokenUsuario", header = true) TokenUsuario token,
+			@WebParam(name = "item") Item item) throws AutorizacaoException {
+		System.out.println("Cadastrando item " + item + ", Token: " + token);
+		
+		boolean valido = new TokenDao().ehValido(token);
+		if(!valido) {
+			throw new AutorizacaoException("Autorizacao falhou");
+		}
+		this.dao.cadastrar(item);
+
+		return item;
+	}
+}
